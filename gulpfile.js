f1f7b30f06572c2b20df19d1ee5aa4c1ad456f11
Adie Williams <hello@adie.codes>
2016-11-19 14:52:01 +0000

    Chore: Add gulp task for resizing and minimising images

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var imageResize = require('gulp-image-resize');
+var rename = require('gulp-rename');
 
 gulp.task('browser-sync', function() {
   browserSync.init({
@@ -9,4 +12,22 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("**/*.html").on('change', browserSync.reload);
   gulp.watch("**/*.css").on('change', browserSync.reload);
+});
+
+gulp.task('images', function(){
+  // Get all images in /images_src
+  gulp.src("images_src/**/*.+(jpg|png)")
+  // Resize images
+  .pipe(imageResize({
+    width: 960,
+    upscale: false
+  }))
+  // Minify images
+  .pipe(imagemin())
+  // Rename images, add appropriate size attributes
+  .pipe(rename({
+    suffix: "-960"
+  }))
+  // Move to /images_dist
+  .pipe(gulp.dest('images_dist'))
 });
