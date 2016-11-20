9be01777ee2f7ef41f8937da996d32bd0c10f6c5
Adie Williams <hello@adie.codes>
2016-11-20 09:36:23 +0000

    Chore: Add build task for multiple image sizes

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var browserSync = require('browser-sync').create();
 var imagemin = require('gulp-imagemin');
 var imageResize = require('gulp-image-resize');
 var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
 
 gulp.task('browser-sync', function() {
   browserSync.init({
@@ -14,7 +15,25 @@ gulp.task('browser-sync', function() {
   gulp.watch("**/*.css").on('change', browserSync.reload);
 });
 
-gulp.task('images', function(){
+gulp.task('images_1920', function(){
+  // Get all images in /images_src
+  gulp.src("images_src/**/*.+(jpg|png)")
+  // Resize images
+  .pipe(imageResize({
+    width: 1920,
+    upscale: false
+  }))
+  // Minify images
+  .pipe(imagemin())
+  // Rename images, add appropriate size attributes
+  .pipe(rename({
+    suffix: "_1920"
+  }))
+  // Move to /images_dist
+  .pipe(gulp.dest('images_dist'))
+});
+
+gulp.task('images_960', function(){
   // Get all images in /images_src
   gulp.src("images_src/**/*.+(jpg|png)")
   // Resize images
@@ -26,8 +45,30 @@ gulp.task('images', function(){
   .pipe(imagemin())
   // Rename images, add appropriate size attributes
   .pipe(rename({
-    suffix: "-960"
+    suffix: "_960"
   }))
   // Move to /images_dist
   .pipe(gulp.dest('images_dist'))
 });
+
+gulp.task('images_480', function(){
+  // Get all images in /images_src
+  gulp.src("images_src/**/*.+(jpg|png)")
+  // Resize images
+  .pipe(imageResize({
+    width: 480,
+    upscale: false
+  }))
+  // Minify images
+  .pipe(imagemin())
+  // Rename images, add appropriate size attributes
+  .pipe(rename({
+    suffix: "_480"
+  }))
+  // Move to /images_dist
+  .pipe(gulp.dest('images_dist'))
+});
+
+gulp.task('build', function(callback){
+  runSequence('images_1920', 'images_960', 'images_480', callback)
+})
